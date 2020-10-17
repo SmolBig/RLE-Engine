a75425f4b88453079eac23baadaed3a76d769522
SmolBig <smolbigrando@gmail.com>
2020-10-16 20:34:05 -0700

    Templatized Node, separated packed node type from working node type.

--- RLE Engine/main.cpp
+++ RLE Engine/main.cpp
@@ -6,36 +6,50 @@
 #include <limits>
 
 //~~@ use memory mapped files instead of file streams
-//~~@ RLE(X) magic where X is node type (high nibble offset byte ct, low nibble length byte ct)
+//~~@ templatize node-accepting functions
+//~~@ RLE(X) header value, where X is node type (high nibble offset byte ct, low nibble length byte ct)
 //~~_ get some better test files
 //~~_ build a test harness
+//~~@ modify struct Run to use span instead of position
+//~~~ check compiler implementations to ensure that packed is behaving properly and that vector is treating it right
 
 using byte = uint8_t;
 
-#pragma pack(push, 1)
-struct RLENode {
-  using OffsetType = uint8_t;
-  using LengthType = uint8_t;
+template <typename OffsetT, typename LengthT>
+struct BaseRLENode {
+  using OffsetType = OffsetT;
+  static constexpr size_t OffsetMax = std::numeric_limits<OffsetType>::max();
+  using LengthType = LengthT;
+  static constexpr size_t LengthMax = std::numeric_limits<LengthType>::max();
 
-  OffsetType offsetFromPreviousNode;
+  OffsetType prefix; //offset from previous node's tail
   LengthType length;
+  byte value;
+};
+
+using RLENode = BaseRLENode<uint32_t, uint32_t>;
+
+#pragma pack(push, 1)
+template <typename OffsetT, typename LengthT>
+struct BasePackedRLENode {
+  OffsetT prefix;
+  LengthT length;
   byte value;
 };
 #pragma pack(pop)
 
-constexpr size_t OffsetMax = std::numeric_limits<RLENode::OffsetType>::max();
-constexpr size_t LengthMax = std::numeric_limits<RLENode::LengthType>::max();
-
-template<class T>
-constexpr size_t bitsizeof() {
-  return sizeof(T) * 8;
-}
+using PackedRLENode = BasePackedRLENode<RLENode::OffsetType, RLENode::LengthType>;
 
 struct Run {
   size_t position;
   size_t length;
   byte value;
 };
+
+template<class T>
+constexpr size_t bitsizeof() {
+  return sizeof(T) * 8;
+}
 
 std::vector<byte> loadFile(const std::string& filename) {
   std::vector<byte> data;
@@ -67,7 +81,7 @@ std::vector<Run> collectRuns(const std::vector<byte>& data) {
       run.length++;
     }
 
-    reuse = (run.length <= sizeof(RLENode));
+    reuse = (run.length <= sizeof(PackedRLENode));
   }
 
   if(reuse) {
@@ -85,11 +99,11 @@ constexpr size_t lengthFromPair(RLENode::OffsetType offset, RLENode::LengthType 
 }
 
 size_t lengthFromLongNode(const RLENode& node) {
-  return lengthFromPair(node.offsetFromPreviousNode, node.length);
+  return lengthFromPair(node.prefix, node.length);
 }
 
 RLENode makeLongNode(size_t length, uint8_t value) {
-  size_t loLength = length & LengthMax;
+  size_t loLength = length & RLENode::LengthMax;
   size_t hiLength = length >> bitsizeof<RLENode::LengthType>();
   return RLENode{ (RLENode::OffsetType)hiLength, (RLENode::LengthType)loLength, value };
 }
@@ -102,28 +116,28 @@ std::vector<RLENode> parseRuns(const std::vector<Run>& runs) {
   for(auto& run : runs) {
     //Ensure that offset from previous tail is short enough for OffsetType
     size_t runOffset = run.position - previousNodeTail;
-    size_t                gaps = runOffset / OffsetMax;
-    RLENode::OffsetType offset = runOffset % OffsetMax;
+    size_t                gaps = runOffset / RLENode::OffsetMax;
+    RLENode::OffsetType offset = runOffset % RLENode::OffsetMax;
     if(gaps) {
       //zero-length node with non-zero value represents "advance tail by (offset * value)"
       while(gaps > std::numeric_limits<byte>::max()) {
-        table.push_back({ OffsetMax, 0, (byte)std::numeric_limits<byte>::max() });
+        table.push_back({ RLENode::OffsetMax, 0, (byte)std::numeric_limits<byte>::max() });
         gaps -= std::numeric_limits<byte>::max();
       }
-      table.push_back({ OffsetMax, 0, (byte)gaps });
+      table.push_back({ RLENode::OffsetMax, 0, (byte)gaps });
     }
 
     size_t length = run.length;
-    if(length > LengthMax) {
-      constexpr size_t LONG_NODE_MAX_LENGTH = lengthFromPair(OffsetMax, LengthMax);
+    if(length > RLENode::LengthMax) {
+      constexpr size_t LONG_NODE_MAX_LENGTH = lengthFromPair(RLENode::OffsetMax, RLENode::LengthMax);
 
       while(length > LONG_NODE_MAX_LENGTH) {
         table.push_back({ offset, 0, 0 });
         offset = 0;
-        table.push_back({ OffsetMax, LengthMax, run.value });
+        table.push_back({ RLENode::OffsetMax, RLENode::LengthMax, run.value });
         length -= LONG_NODE_MAX_LENGTH;
       }
-      if(length > LengthMax) {
+      if(length > RLENode::LengthMax) {
         table.push_back({ offset, 0, 0 });
         offset = 0;
         table.push_back(makeLongNode(length, run.value));
@@ -140,23 +154,43 @@ std::vector<RLENode> parseRuns(const std::vector<Run>& runs) {
   return table;
 }
 
-int64_t checkEfficiency(const std::vector<RLENode>& table) {
-  size_t tableLength = table.size() * sizeof(RLENode);
+int64_t checkTableBytesSaved(const std::vector<RLENode>& table) {
+  size_t tableLength = table.size() * sizeof(PackedRLENode);
   size_t totalRunsLength = 0;
   for(auto& node : table) { totalRunsLength += node.length; }
   return totalRunsLength - tableLength;
+}
+
+std::vector<PackedRLENode> packTable(const std::vector<RLENode>& unpackedTable) {
+  std::vector<PackedRLENode> packedTable;
+  packedTable.reserve(unpackedTable.size());
+  for(auto& node : unpackedTable) {
+    packedTable.push_back({ node.prefix, node.length, node.value });
+  }
+  return packedTable;
+}
+
+std::vector<RLENode> unpackTable(const std::vector<PackedRLENode>& packedTable) {
+  std::vector<RLENode> unpackedTable;
+  unpackedTable.reserve(packedTable.size());
+  for(auto& node : packedTable) {
+    unpackedTable.push_back({ node.prefix, node.length, node.value });
+  }
+  return unpackedTable;
 }
 
 void writeDeflatedFile(const std::vector<byte>& data, const std::vector<RLENode>& table, const std::string& filename) {
   auto file = std::ofstream(filename, std::ios::binary);
   if(!file) { throw std::runtime_error("Could not open output file."); }
   
-  if(table.size() > std::numeric_limits<uint32_t>::max()) {
+  size_t tsz = table.size();
+  if(tsz > std::numeric_limits<uint32_t>::max()) {
     throw std::runtime_error("Table size too large. (Insanely large. What on earth are you deflating?");
   }
-  uint32_t tableNodeCount = (uint32_t)table.size();
+  uint32_t tableNodeCount = (uint32_t)tsz;
   file.write(reinterpret_cast<const char*>(&tableNodeCount), sizeof(tableNodeCount));
-  file.write(reinterpret_cast<const char*>(table.data()), (std::streamsize)tableNodeCount * sizeof(std::remove_reference<decltype(table)>::type::value_type));
+  auto packedTable = packTable(table);
+  file.write(reinterpret_cast<const char*>(packedTable.data()), (std::streamsize)tableNodeCount * sizeof(std::remove_reference<decltype(table)>::type::value_type));
 
   //begin writing deflated data
   const char* dataPtr = reinterpret_cast<const char*>(data.data());
@@ -169,7 +203,7 @@ void writeDeflatedFile(const std::vector<byte>& data, const std::vector<RLENode>
       continue;
     }
 
-    size_t bytesToCopy = node.offsetFromPreviousNode;
+    size_t bytesToCopy = node.prefix;
     if(node.length == 0) {
       if(node.value == 0) {
         longNode = true;
@@ -191,16 +225,19 @@ void inflateFile(const std::string& srcFilename, const std::string& dstFilename)
   if(!src) { throw std::runtime_error("Could not open source file."); }
   auto dst = std::ofstream(dstFilename, std::ios::binary);
   if(!dst) { throw std::runtime_error("Could not open destination file."); }
+
   //determine length of source file
   src.seekg(0, std::ios::end);
   size_t srcLength = (size_t)src.tellg();
   src.seekg(0, std::ios::beg);
+
   //read in RLE table
   uint32_t tableNodeCount;
   src.read(reinterpret_cast<char*>(&tableNodeCount), sizeof(tableNodeCount));
   if(!src.good()) { throw "derp"; }
-  std::vector<RLENode> table(tableNodeCount);
-  src.read(reinterpret_cast<char*>(table.data()), (std::streamsize)tableNodeCount * sizeof(decltype(table)::value_type));
+  std::vector<PackedRLENode> packedTable(tableNodeCount);
+  src.read(reinterpret_cast<char*>(packedTable.data()), (std::streamsize)tableNodeCount * sizeof(decltype(packedTable)::value_type));
+  auto table = unpackTable(packedTable);
 
   //process nodes
   bool longNode = false;
@@ -214,7 +251,7 @@ void inflateFile(const std::string& srcFilename, const std::string& dstFilename)
       continue;
     }
 
-    size_t bytesToCopy = node.offsetFromPreviousNode;
+    size_t bytesToCopy = node.prefix;
     if(node.length == 0) {
       if(node.value == 0) {
         longNode = true;
@@ -240,10 +277,16 @@ void inflateFile(const std::string& srcFilename, const std::string& dstFilename)
 }
 
 int main() {
+  std::cout << sizeof(RLENode) << "\n";
+  std::cout << sizeof(PackedRLENode) << "\n";
+  system("pause");
+
+#ifdef HERPDERP
   auto data = loadFile("testfile.txt");
   auto runs = collectRuns(data);
   auto table = parseRuns(runs);
   
   writeDeflatedFile(data, table, "deflated.rle");
   inflateFile("deflated.rle", "inflated.txt");
+#endif
 }
