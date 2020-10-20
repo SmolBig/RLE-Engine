4072b5ce3725c3a14d792eb2b77a59b394ab77d3
SmolBig <smolbigrando@gmail.com>
2020-10-19 21:24:53 -0700

    Added RAII class and used it to implement exception safety for MappedFile ctor; Commented MappedFile

--- RLE Engine/MappedFile.cpp
+++ RLE Engine/MappedFile.cpp
@@ -3,7 +3,7 @@
 #include <Windows.h>
 #include <stdexcept>
 
-//~~#
+// Simple utility function which throws a std::runtime_error with the error message generated from WinAPI.
 void throwWindowsError() {
   LPSTR buffer = NULL;
   FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER, NULL, GetLastError(), 0, (LPSTR)&buffer, 0, NULL);
@@ -12,8 +12,36 @@ void throwWindowsError() {
   throw std::runtime_error(msg);
 }
 
-//~~#
+// Simple RAII wrapper for exception safety in MappedFile constructor
+//
+// Releases assigned handle on destruction unless commit() has been called, 
+//   or handle value is NULL or INVALID_HANDLE_VALUE.
+class RAIIHandle {
+public:
+  RAIIHandle(HANDLE h) { handle = h; }
+  void operator=(HANDLE h) { handle = h; }
+
+  operator HANDLE() { return handle; }
+
+  HANDLE commit() {
+    releaseOnDestruct = false;
+    return handle;
+  }
+
+  ~RAIIHandle() {
+    if(releaseOnDestruct && (handle != NULL) && (handle != INVALID_HANDLE_VALUE)) {
+      CloseHandle(handle); 
+    }
+  }
+
+private:
+  bool releaseOnDestruct = true;
+  HANDLE handle = NULL;
+
+};
+
 MappedFile::MappedFile(const std::string& filename, CreationDisposition disposition, uint64_t desiredLength) {
+  // Translate from enum to CreateFile argument and ensure that newly created files have non-zero length.
   DWORD disp = OPEN_EXISTING;
   if(disposition == CreationDisposition::CREATE) {
     disp = CREATE_NEW;
@@ -22,26 +50,32 @@ MappedFile::MappedFile(const std::string& filename, CreationDisposition disposit
     }
   }
 
-  file = CreateFileA(filename.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, disp, 0, NULL);
-  if(file == INVALID_HANDLE_VALUE) { throwWindowsError(); }
+  // Generate file
+  RAIIHandle hFile = CreateFileA(filename.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, disp, 0, NULL);
+  if(hFile == INVALID_HANDLE_VALUE) { throwWindowsError(); }
 
+  // Map file
   LARGE_INTEGER size;
   size.QuadPart = desiredLength;
-  map = CreateFileMappingA(file, NULL, PAGE_READWRITE, size.HighPart, size.LowPart, NULL);
-  if(map == nullptr) { throwWindowsError(); }
+  RAIIHandle hMap = CreateFileMappingA(file, NULL, PAGE_READWRITE, size.HighPart, size.LowPart, NULL);
+  if(hMap == nullptr) { throwWindowsError(); }
 
+  // Since mapping with a length exceeding the file length will grow the file, fetch
+  //   the filesize at this point to serve as the actual length of the file.
   GetFileSizeEx(file, &size);
   length = size.QuadPart;
+  file = hFile.commit();
+  map = hMap.commit();
 }
 
-//~~#
 MappedFile::~MappedFile() {
   CloseHandle(map);
   CloseHandle(file);
 }
 
-//~~#
 MappedFile::View MappedFile::getView(uint64_t offset, size_t viewLength) {
+  // For some reason intellisense really hates this function, and commonly reports
+  //   errors which do not actually exist.
   LARGE_INTEGER liOffset;
   liOffset.QuadPart = offset;
   void* ptr = MapViewOfFile(map, FILE_MAP_WRITE, liOffset.HighPart, liOffset.LowPart, viewLength);
@@ -49,15 +83,13 @@ MappedFile::View MappedFile::getView(uint64_t offset, size_t viewLength) {
   return View(reinterpret_cast<byte*>(ptr), viewLength);
 }
 
-//~~#
 MappedFile::View::View(byte* data, size_t length) :
-  ptr(data),
-  std::span<byte>(data, length)
+  std::span<byte>(data, length), //note that this ctor executes first, regardless of list order
+  ptr(data)
 {
   //nop
 }
 
-//~~#
 MappedFile::View::~View() {
   FlushViewOfFile(ptr, 0);
   UnmapViewOfFile(ptr);
--- RLE Engine/MappedFile.h
+++ RLE Engine/MappedFile.h
@@ -2,29 +2,49 @@
 #include <span>
 #include <string>
 
-//~~#
+/// <summary>
+/// class MappedFile
+/// Opens a file and memory maps it using the Win32 API.
+/// Provides an interface for generating views for the mapped file.
+/// 
+/// This class can be used to open or create files which can be read from
+///   and written to as if they were sections of main memory.
+/// </summary>
 class MappedFile {
+  // Stand-in for std::byte until that type is better integrated.
   using byte = unsigned char;
 
 public:
   enum class CreationDisposition {
-    OPEN, //~~#
-    CREATE //~~#
+    OPEN, // Will open an existing file or throw a std::runtime_error if the file is not found.
+    CREATE // Will create a new file or throw a std::runtime_error if the file already exists.
+    //~~_ add an option for "open or create"?
   };
 
-  //~~#
+  // MappedFile constructor
+  // When disposition is OPEN, length is ignored.
+  // When disposition is CREATE, length must be non-zero.
+  // Map length can not be adjusted after creation.
   MappedFile(const std::string& filename, CreationDisposition disposition, uint64_t desiredLength = 0);
   ~MappedFile();
 
-  //~~#
+  // Returns length (in bytes) of file/map.
   uint64_t size() const { return length; }
 
-  class View; //forward declaration
+  class View; // forward declaration
 
-  //~~#
+  // Returns a View object with the indicated offset and length.
+  // Providing a length of zero will attempt to return a view from
+  //   the offset to the end of the file/map.
   View getView(uint64_t offset, size_t length);
 
-  //~~#
+  // class MappedFile::View
+  // Objects of this type must be instantiated using the MappedFile::getView() function.
+  // The View object inherits from std::span<byte>. The only additional behavior is a
+  //   destructor which flushes and releases the underlying WinAPI view resource.
+  // Note that views are invalidated when the MappedFile object which created them is
+  //   destructed. View behavior beyond that point is undefined, but will probably
+  //   (hopefully) result in a segfault.
   class View : public std::span<byte> {
     friend View MappedFile::getView(uint64_t offset, size_t length);
   public:
@@ -33,6 +53,7 @@ public:
   private:
     View(byte*, size_t);
     byte* ptr;
+
   };
 
 private:
