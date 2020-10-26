a1b13514af15641fc989c73f50570e4634d82df3
SmolBig <smolbigrando@gmail.com>
2020-10-25 23:19:43 -0700

    Save point near completion of refactor.

--- RLE Engine/Source.cpp
+++ RLE Engine/Source.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <limits>
 #include <stdexcept>
+#include <unordered_map>
 #include "MappedFile.h"
 
 #pragma warning(disable : 4458)
@@ -22,6 +23,7 @@ enum class NodeFormat {
   P8L16  = 0x12,
   P16L8  = 0x21,
   P16L16 = 0x22,
+  INEFFICIENT
 };
 
 #pragma pack(push, 1)
@@ -109,13 +111,13 @@ struct PackedNode {
 
   uint64_t getLongLength() const {
     uint64_t loBits = length;
-    uint64_t hiBits = prefix << bitsizeof(length);
+    uint64_t hiBits = ((uint64_t)prefix) << bitsizeof(length);
     return loBits | hiBits;
   }
 
   uint64_t getSkipLength() const {
     uint64_t loBits = prefix;
-    uint64_t hiBits = value << bitsizeof(prefix);
+    uint64_t hiBits = ((uint64_t)value) << bitsizeof(prefix);
     return loBits | hiBits;
   }
 };
@@ -171,77 +173,170 @@ int64_t measureEfficiency(const std::vector<NodeType>& nodes) {
 }
 
 struct RLETable {
+  RLETable() = default;
+
   template <class NodeType>
-  RLETable(NodeFormat format, const std::vector<NodeType>& nodes) : 
+  RLETable(NodeFormat format, int64_t efficiency, const std::vector<NodeType>& nodes) : 
     format(format),
-    nodeCount(nodes.size())
+    efficiency(efficiency),
+    nodeCount((uint32_t)nodes.size())
   {
-    auto begin = (const std::byte*)nodes.data();
-    auto end = begin + std::span(nodes).size_bytes();
-    std::span<std::byte> span(begin, end);
+    if(nodes.size() > std::numeric_limits<uint32_t>::max()) {
+      throw std::runtime_error("RLE table too large.");
+    }
+    std::span<const std::byte> span(reinterpret_cast<const std::byte*>(nodes.data()), std::span(nodes).size_bytes());
     nodesAsBytes.insert(nodesAsBytes.begin(), span.begin(), span.end());
   }
 
   NodeFormat format;
-  size_t nodeCount;
+  int64_t efficiency;
+  uint32_t nodeCount;
   std::vector<std::byte> nodesAsBytes;
 };
 
-RLETable selectFormatAndGenerateRLETable(const std::vector<Run>& runs) {
-  //~~@ This is horrible. Make a function that checks efficiency
-  //mathematically from runs vector prior to generating a table.
-
-  std::vector<PackedNode<uint8_t,  uint8_t>>   nodes8x8;
-  std::vector<PackedNode<uint8_t,  uint16_t>>  nodes8x16;
-  std::vector<PackedNode<uint16_t, uint8_t>>   nodes16x8;
-  std::vector<PackedNode<uint16_t, uint16_t>>  nodes16x16;
-
-  for(auto& run : runs) { parseRun(run, nodes8x8); }
-  for(auto& run : runs) { parseRun(run, nodes8x16); }
-  for(auto& run : runs) { parseRun(run, nodes16x8); }
-  for(auto& run : runs) { parseRun(run, nodes16x16); }
-  
-  auto efficiency8x8   = measureEfficiency(nodes8x8);
-  auto efficiency8x16  = measureEfficiency(nodes8x16);
-  auto efficiency16x8  = measureEfficiency(nodes16x8);
-  auto efficiency16x16 = measureEfficiency(nodes16x16);
-
-  NodeFormat bestFormat = NodeFormat::P8L8;
-  auto bestEfficiency = efficiency8x8;
-  if(efficiency8x16 > bestEfficiency) {
-    bestFormat = NodeFormat::P8L16;
-    bestEfficiency = efficiency8x16;
-  }
-  if(efficiency16x8 > bestEfficiency) {
-    bestFormat = NodeFormat::P16L8;
-    bestEfficiency = efficiency16x8;
-  }
-  if(efficiency16x16 > bestEfficiency) {
-    bestFormat = NodeFormat::P16L16;
-    bestEfficiency = efficiency16x16;
-  }
-
-  switch(bestFormat) {
-  case NodeFormat::P8L8:   return RLETable(bestFormat, nodes8x8);
-  case NodeFormat::P8L16:  return RLETable(bestFormat, nodes8x16);
-  case NodeFormat::P16L8:  return RLETable(bestFormat, nodes16x8);
-  case NodeFormat::P16L16: return RLETable(bestFormat, nodes16x16);
-  }
-
-  throw std::logic_error("Failed to return from available options.");
+template <class NodeType>
+int64_t measureFormatEfficiency(const std::vector<Run>& runs) {
+  int64_t efficiency = 0;
+  runs; //~~_
+  return efficiency;
 }
 
-NodeFormat selectFormatMathemagically(const std::vector<Run>& runs) {
-  //~~_
-  return NodeFormat::P8L8; //~~! placeholder
+using Node8x8   = PackedNode<uint8_t,  uint8_t>;
+using Node8x16  = PackedNode<uint8_t,  uint16_t>;
+using Node16x8  = PackedNode<uint16_t, uint8_t>;
+using Node16x16 = PackedNode<uint16_t, uint16_t>;
+
+std::pair<NodeFormat, int64_t> selectFormat(const std::vector<Run>& runs) {
+  std::unordered_map<NodeFormat, int64_t> efficiencies{
+    { NodeFormat::P8L8,   measureFormatEfficiency<Node8x8>(runs) },
+    { NodeFormat::P8L16,  measureFormatEfficiency<Node8x16>(runs) },
+    { NodeFormat::P16L8,  measureFormatEfficiency<Node16x8>(runs) },
+    { NodeFormat::P16L16, measureFormatEfficiency<Node16x16>(runs) }
+  };
+
+  int64_t bestEfficiency = 0;
+  NodeFormat bestFormat = NodeFormat::INEFFICIENT;
+  for(auto p : efficiencies) {
+    if(p.second > bestEfficiency) {
+      bestEfficiency = p.second;
+      bestFormat = p.first;
+    }
+  }
+
+  return std::make_pair(bestFormat, bestEfficiency);
 }
 
 template <class NodeType>
-int64_t measureFormatEfficiency(const std::vector<Run>& runs) {
-  //~~_
-  return 0; //~~! placeholder
+RLETable generateRLETable(NodeFormat format, int64_t efficiency, const std::vector<Run>& runs) {
+  std::vector<NodeType> nodes;
+  for(auto& run : runs) { parseRun(run, nodes); }
+  return RLETable(format, efficiency, nodes);
 }
 
+void reject() {
+  throw std::runtime_error("Cannot deflate this file efficiently. Reject it. (Rejection not yet implemented.)");
+}
 
+template <class NodeType>
+void deflateData(MappedFile::View& inView, MappedFile::View& outView) {
+  Header* header = reinterpret_cast<Header*>(outView.data());
+  const NodeType* nodesPtr = reinterpret_cast<const NodeType*>(outView.data() + sizeof(Header));
+  std::span<const NodeType> nodes(nodesPtr, header->tableNodeCount);
 
+  auto inIter = inView.begin();
+  auto outIter = outView.begin();
+  outIter += sizeof(Header);
+  outIter += nodes.size_bytes();
 
+  bool longNode = false;
+  for(auto& node : nodes) {
+    if(longNode) {
+      inIter += node.getLongLength();
+      longNode = false;
+      continue;
+    }
+
+    size_t prefix = node.prefix;
+    if(node.length == 0) {
+      if(node.value == (std::byte)0) {
+        longNode = true;
+      }
+      else {
+        prefix = node.getSkipLength();
+      }
+    }
+
+    auto tailIter = inIter + prefix;
+    outIter = std::copy(inIter, tailIter, outIter);
+    //tailIter is past the prefix, add the run length to that to get the new srcIter
+    inIter = tailIter + node.length;
+  }
+
+  std::copy(inIter, inView.end(), outIter);
+}
+
+std::vector<Run> collectRuns(const std::span<std::byte>& data) {
+  std::vector<Run> runs;
+
+  Run run;
+  size_t prevTailPos = 0;
+  for(size_t i = 0; i < data.size(); ) {
+    auto position = i;
+    run.length = 1;
+    run.value = data[i];
+
+    while((++i < data.size()) && (data[i] == run.value) && (run.length < std::numeric_limits<decltype(run.length)>::max())) {
+      run.length++;
+    }
+
+    if(run.length > sizeof(Node8x8)) {
+      run.prefix = position - prevTailPos;
+      runs.push_back(run);
+      prevTailPos = position + run.length;
+    }
+  }
+
+  return runs;
+}
+
+void deflateFile(const std::string& inputFilename, const std::string& outputFilename) {
+  MappedFile inMap(inputFilename, MappedFile::CreationDisposition::OPEN);
+  auto inView = inMap.getView(0, inMap.size());
+
+  std::vector<Run> runs = collectRuns(inView);
+
+  auto selection = selectFormat(runs);
+  auto format = selection.first;
+  auto efficiency = selection.second;
+
+  RLETable table;
+  switch(format) {
+  case NodeFormat::P8L8:   table = generateRLETable<Node8x8  >(format, efficiency, runs); break;
+  case NodeFormat::P8L16:  table = generateRLETable<Node8x16 >(format, efficiency, runs); break;
+  case NodeFormat::P16L8:  table = generateRLETable<Node16x8 >(format, efficiency, runs); break;
+  case NodeFormat::P16L16: table = generateRLETable<Node16x16>(format, efficiency, runs); break;
+  case NodeFormat::INEFFICIENT:
+    reject();
+    return;
+  };
+
+  uint64_t compressedLength = inMap.size() - table.efficiency;
+  MappedFile outMap(outputFilename, MappedFile::CreationDisposition::CREATE, compressedLength);
+  auto outView = outMap.getView(0, outMap.size());
+
+  Header* header = new(outView.data()) Header;
+  header->setNodeFormat(format);
+  header->decompressedLength = inMap.size();
+  header->tableNodeCount = table.nodeCount;
+
+  auto outIter = outView.begin() + sizeof(Header);
+  std::copy(table.nodesAsBytes.begin(), table.nodesAsBytes.end(), outIter);
+
+  switch(format) {
+  case NodeFormat::P8L8:   deflateData<Node8x8  >(inView, outView); break;
+  case NodeFormat::P8L16:  deflateData<Node8x16 >(inView, outView); break;
+  case NodeFormat::P16L8:  deflateData<Node16x8 >(inView, outView); break;
+  case NodeFormat::P16L16: deflateData<Node16x16>(inView, outView); break;
+  default: throw std::logic_error("Failed switch to format.");
+  }
+}
